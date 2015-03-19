1970bb0b1195a0b08fcd29a7881c8f408b546d8a
jlthomps <jlthomps@usgs.gov>
2015-03-19 14:03:01 -0500

    fix for error on no complete water years

--- R/getDataUSGS.R
+++ R/getDataUSGS.R
@@ -32,6 +32,9 @@ getDataUSGS <- function(sites,startdate,enddate) {
       obs_data <- get_obsdata(x_obs)
       obs_count<-nrow(obs_data)
       cat(paste("get_obsdata run on x_obs for site",site,obs_count,"\n",sep=" "))
+      if (obs_count==0) {
+        comment<-"No complete water years for site"
+      } else {
       countbyyr<-aggregate(obs_data$discharge, list(obs_data$wy_val), length)
       colnames(countbyyr)<-c('wy','num_samples')
       sub_countbyyr<-countbyyr[countbyyr$num_samples>=365,]
@@ -43,7 +46,7 @@ getDataUSGS <- function(sites,startdate,enddate) {
         obs_data <- obs_data[,c('wy_val','date','discharge','month_val','year_val','day_val','jul_val')]
         site_no <- rep(site,nrow(obs_data))
         obs_data <- cbind(site_no,obs_data,stringsAsFactors=FALSE)
-      }} else {
+      }}} else {
         obs_data<-"No observed data for this site"
       }
     dataOut[[i]]<-obs_data
