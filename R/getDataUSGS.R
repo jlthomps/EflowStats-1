ffd81114ae9733ccf4f4a002ee32cfb880aec9fe
jlthomps <jlthomps@usgs.gov>
2015-04-24 14:59:38 -0500

    added check to getDataUSGS

--- R/getDataUSGS.R
+++ R/getDataUSGS.R
@@ -33,22 +33,24 @@ getDataUSGS <- function(sites,startdate,enddate) {
       obs_count<-nrow(obs_data)
       cat(paste("get_obsdata run on x_obs for site",site,obs_count,"\n",sep=" "))
       if (obs_count==0) {
-        comment<-"No complete water years for site"
+        cat(paste("No complete water years for site",site,"\n",sep=" ")
       } else {
-      countbyyr<-aggregate(obs_data$discharge, list(obs_data$wy_val), length)
-      colnames(countbyyr)<-c('wy','num_samples')
-      sub_countbyyr<-countbyyr[countbyyr$num_samples>=365,]
-      if (nrow(sub_countbyyr)==0) {
-        comment<-"No complete water years for site"
-      } else {
-        obs_data<-merge(obs_data,sub_countbyyr,by.x="wy_val",by.y="wy")
-        obs_data<-obs_data[order(obs_data$date),]
-        obs_data <- obs_data[,c('wy_val','date','discharge','month_val','year_val','day_val','jul_val')]
-        site_no <- rep(site,nrow(obs_data))
-        obs_data <- cbind(site_no,obs_data,stringsAsFactors=FALSE)
-      }}} else {
-        obs_data<-"No observed data for this site"
+        countbyyr<-aggregate(obs_data$discharge, list(obs_data$wy_val), length)
+        colnames(countbyyr)<-c('wy','num_samples')
+        sub_countbyyr<-countbyyr[countbyyr$num_samples>=365,]
+        if (nrow(sub_countbyyr)==0) {
+          cat(paste("No complete water years for site",site,"\n",sep=" ")
+        } else {
+          obs_data<-merge(obs_data,sub_countbyyr,by.x="wy_val",by.y="wy")
+          obs_data<-obs_data[order(obs_data$date),]
+          obs_data <- obs_data[,c('wy_val','date','discharge','month_val','year_val','day_val','jul_val')]
+          site_no <- rep(site,nrow(obs_data))
+          obs_data <- cbind(site_no,obs_data,stringsAsFactors=FALSE)
+        }
       }
+    } else {
+        obs_data<-"No observed data for this site"
+    }
     dataOut[[i]]<-obs_data
   }
   return(dataOut)
